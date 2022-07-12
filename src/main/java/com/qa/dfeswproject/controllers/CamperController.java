883e135e701778beef4a4cdaac17fdb99b947a0b
sharonpeevor <shazpeevor@hotmail.com>
2022-07-12 14:47:12 +0100

    wrote create method

--- src/main/java/com/qa/dfeswproject/controllers/CamperController.java
+++ src/main/java/com/qa/dfeswproject/controllers/CamperController.java
@@ -1,7 +1,11 @@
 package com.qa.dfeswproject.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.dfeswproject.entities.Camper;
 
 @RestController
 @RequestMapping("/camper")
@@ -13,5 +17,8 @@ public class CamperController {
 		this.service = service;
 	}
 	
-	
+	@PostMapping("/create")
+	public Camper create(@RequestBody Camper camper) {
+		return this.service.create(camper);
+	}
 }
