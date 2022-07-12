b865957fc50886519e87aba121cdff1629805f7a
sharonpeevor <shazpeevor@hotmail.com>
2022-07-12 11:52:11 +0100

    entity class

--- src/main/java/com/qa/dfeswproject/entities/Camper.java
+++ src/main/java/com/qa/dfeswproject/entities/Camper.java
@@ -1,5 +1,103 @@
 package com.qa.dfeswproject.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Camper {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String lastName;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String unit;
+	
+	@Column(nullable = false)
+	private LocalDate arrivalDate;
+	
+	@Column(nullable = false)
+	private int noOfNights;
+
+	public Camper() {}
+
+	public Camper(String lastName, String email, String unit, LocalDate arrivalDate, int noOfNights) {
+		super();
+		this.lastName = lastName;
+		this.email = email;
+		this.unit = unit;
+		this.arrivalDate = arrivalDate;
+		this.noOfNights = noOfNights;
+	}
+
+	public Camper(long id, String lastName, String email, String unit, LocalDate arrivalDate, int noOfNights) {
+		super();
+		this.id = id;
+		this.lastName = lastName;
+		this.email = email;
+		this.unit = unit;
+		this.arrivalDate = arrivalDate;
+		this.noOfNights = noOfNights;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public LocalDate getArrivalDate() {
+		return arrivalDate;
+	}
+
+	public void setArrivalDate(LocalDate arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+
+	public int getNoOfNights() {
+		return noOfNights;
+	}
+
+	public void setNoOfNights(int noOfNights) {
+		this.noOfNights = noOfNights;
+	}
+	
+	
 }
