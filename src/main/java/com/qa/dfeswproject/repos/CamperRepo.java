12a7105d8d6ad3cf6cc8363ac8aad99af005570a
sharonpeevor <shazpeevor@hotmail.com>
2022-07-12 16:17:04 +0100

    configure repo interface

--- src/main/java/com/qa/dfeswproject/repos/CamperRepo.java
+++ src/main/java/com/qa/dfeswproject/repos/CamperRepo.java
@@ -1,5 +1,11 @@
 package com.qa.dfeswproject.repos;
 
-public class CamperRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.dfeswproject.entities.Camper;
+
+@Repository
+public interface CamperRepo extends JpaRepository<Camper, Long> {
 
 }
