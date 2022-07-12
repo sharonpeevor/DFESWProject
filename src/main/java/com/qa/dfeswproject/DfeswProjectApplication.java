0d97f1822b6059d5743da961fd6dc038c4aebabc
sharonpeevor <shazpeevor@hotmail.com>
2022-07-12 10:26:22 +0100

    first commit to dev

---
+++ src/main/java/com/qa/dfeswproject/DfeswProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfeswproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeswProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeswProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dfeswproject/DfeswProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfeswproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeswProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
