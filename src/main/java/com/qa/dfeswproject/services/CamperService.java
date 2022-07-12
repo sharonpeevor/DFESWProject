be865e34e81cf604ff0e1c4b539385520c6eebff
sharonpeevor <shazpeevor@hotmail.com>
2022-07-12 15:33:56 +0100

    write service class methods

--- src/main/java/com/qa/dfeswproject/services/CamperService.java
+++ src/main/java/com/qa/dfeswproject/services/CamperService.java
@@ -1,6 +1,10 @@
 package com.qa.dfeswproject.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.qa.dfeswproject.entities.Camper;
 
 @Service
 public class CamperService {
@@ -11,5 +15,27 @@ public class CamperService {
 		this.repo = repo;
 	}
 	
+	public Camper create(Camper camper) {
+		return this.repo.saveAndFlush(camper);
+	}
 	
+	public List<Camper> readAll() {
+		return this.repo.findAll();
+	}
+	
+	public Camper readById(long id) {
+		return this.repo.findById(id).get();
+	}
+	
+	public Camper update(long id, Camper camper) {
+		Camper existing = this.repo.findById(id).get();
+		
+		existing.setLastName(camper.getLastName());
+		existing.setEmail(camper.getEmail());
+		existing.setUnit(camper.getUnit());
+		existing.setArrivalDate(camper.getArrivalDate());
+		existing.setNoOfNights(camper.getNoOfNights());
+		
+		return this.repo.saveAndFlush(existing);
+	}
 }
