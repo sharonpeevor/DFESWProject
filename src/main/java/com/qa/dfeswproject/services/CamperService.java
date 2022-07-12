c05e67cdbeb5b7e49694896e22bdbd811a2e4a78
sharonpeevor <shazpeevor@hotmail.com>
2022-07-12 15:23:52 +0100

    added constructor

--- src/main/java/com/qa/dfeswproject/services/CamperService.java
+++ src/main/java/com/qa/dfeswproject/services/CamperService.java
@@ -1,5 +1,15 @@
 package com.qa.dfeswproject.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CamperService {
 
+	private CamperRepo repo;
+
+	public CamperService(CamperRepo repo) {
+		this.repo = repo;
+	}
+	
+	
 }
