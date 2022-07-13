8416052d41d26cbca261437d220a1c807e4c6ab6
sharonpeevor <shazpeevor@hotmail.com>
2022-07-13 12:15:30 +0100

    start writing testing methods

--- src/test/java/com/qa/dfeswproject/controllers/CamperControllerTest.java
+++ src/test/java/com/qa/dfeswproject/controllers/CamperControllerTest.java
@@ -1,5 +1,45 @@
 package com.qa.dfeswproject.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.dfeswproject.entities.Camper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class CamperControllerTest {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	@Test
+	public void readAllTest() throws Exception {
+		List<Camper> output = new ArrayList<>();
+		Camper entry = new Camper(1L, "Peevor", "shazpeevor@hotmail.com", "campervan", "16/07/22", 3);
+		output.add(entry);
+		String outputAsJSON = mapper.writeValueAsString(output);
+		
+		mvc.perform(get("/camper/readAll")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(outputAsJSON));
+	}
 }
